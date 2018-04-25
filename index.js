b11c50f60c767bd8e07e9ddd1f84dcab489cd01b
chris hansen <hansen.c.ralph@gmail.com>
2018-04-25 12:35:35 -0400

    copied files

--- index.js
+++ index.js
@@ -74,6 +74,6 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(8000, function(){
-	console.log('listening on *:8000');
+http.listen(8080, function(){
+	console.log('listening on *:8080');
 });
